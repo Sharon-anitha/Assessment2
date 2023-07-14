41e58cf10c35ca769d34d7187989e3317e9c0501
SHARON M <msharon1911@gmail.com>
2023-07-14 16:43:34 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter , Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Landing from './components/Landing';
 import Newticket from './components/Newticket';
-import NotFound from './components/NotFound';
+
 const App = () => {
   return (
     <div className="project">
@@ -13,7 +13,7 @@ const App = () => {
         <Route exact path="/Assessment2" element={<Login />} />
         <Route path="/landing/:name" element={<Landing />} />
         <Route path="/new-ticket" element={<Newticket />} />
-        <Route  path ="*" component={<NotFound/>} />
+       
       </Routes>
     </BrowserRouter>
     </div>
