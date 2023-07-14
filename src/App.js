b2f248c4e668e86057865d9607a070921dd22beb
SHARON M <msharon1911@gmail.com>
2023-07-14 13:38:51 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Landing from './components/Landing';
 import Newticket from './components/Newticket';
-import NotFound from './components/NotFound';
+
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route exact path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/landing/:name" element={<Landing />} />
         <Route path="/new-ticket" element={<Newticket />} />
-        <Route path="*" element={<NotFound />}/>
+        
       </Routes>
     </Router>
   );
--- src/components/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import './NotFound.css'
-const NotFound = () => {
-  return <h2 >404 Not Found</h2>;
-};
-
-export default NotFound;
