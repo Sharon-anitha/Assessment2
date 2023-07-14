d608f02c8a0144578b5c0cc86e23b57b3b0816b6
SHARON M <msharon1911@gmail.com>
2023-07-14 15:35:30 +0530

    h

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Landing from './components/Landing';
 import Newticket from './components/Newticket';
-
+import NotFound from './components/NotFound';
 const App = () => {
   return (
     <Router basename="/login">
