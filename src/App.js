6432c4e45f5cb78c81c5f5243807b96453543c5c
SHARON M <msharon1911@gmail.com>
2023-07-14 15:45:58 +0530

    message

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ import Newticket from './components/Newticket';
 import NotFound from './components/NotFound';
 const App = () => {
   return (
-    <Router basename="/login">
+    
+    <Router basename="/Assessment2">
       <Routes>
-        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/Assessment2" element={<Login />} />
         <Route path="/landing/:name" element={<Landing />} />
         <Route path="/new-ticket" element={<Newticket />} />
         <Route  path ="*" component={<NotFound/>} />
